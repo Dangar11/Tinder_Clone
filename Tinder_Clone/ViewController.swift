c2ca2975e8eba169504d86023faa7cf74c5ad128
dangar11 <tkach.igor.vladimirovich@gmail.com>
2019-01-08 18:21:06 +0200

    first commit

---
+++ Tinder_Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tinder_Clone
+//
+//  Created by Igor Tkach on 1/8/19.
+//  Copyright © 2019 Igor Tkach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
